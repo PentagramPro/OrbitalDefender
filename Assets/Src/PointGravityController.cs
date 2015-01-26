68397eb3a87b3e398790f4ba0aa4c1c4142dc0ef
PentagramPro <maxon-line@yandex.ru>
2015-01-27 02:29:06 +0300

    Scene setup

---
+++ Assets/Src/PointGravityController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PointGravityController : MonoBehaviour {
+
+	List<Transform> Planets = new List<Transform>();
+	float maxGravDist = 500;
+	float maxGravity = 35.0f;
+	// Use this for initialization
+	void Start () {
+	
+		GameObject[] p = GameObject.FindGameObjectsWithTag("Planet");
+		foreach(GameObject o in p)
+		{
+			Planets.Add(o.GetComponent<Transform>());
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		foreach(Transform planet in Planets)
+		{
+			float dist = Vector3.Distance(planet.position, transform.position);
+			if (dist <= maxGravDist) {
+				Vector3 v = planet.position - transform.position;
+				rigidbody2D.AddForce(v.normalized * (1.0f - dist / maxGravDist) * maxGravity);
+			}
+		}
+	}
+}
